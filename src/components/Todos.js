4ed77625529b0c0e17cd38e999abe1cc79e759ce
rortega <rortega@mms.org>
2020-10-26 15:24:13 -0400

    CrashCourse-TodoList_Step1

---
+++ src/components/Todos.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  render() {
+    return  this.props.todos.map((todo) => (
+        <TodoItem  key = { todo.id } markComplete = 
+        { this.props.markComplete } todo = {todo} delTodo={ this.props.delTodo }/>
+
+    ));
+  }
+}
+//PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodos: PropTypes.func.isRequired,
+}
+export default Todos;
+
---
+++ src/components/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+    return (
+    <React.Fragment>
+        <h1>This is my first react project. It's apart of a crash course.</h1>
+    </React.Fragment>
+    )
+}
